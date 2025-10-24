1214c80b113fd12f103fe8b301a902042d35cef6
Max <max.aprende.rapido@gmail.com>
2025-10-24 11:31:59 -0400

    correct don addrss

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 export const CONFIG = {
     API_ENDPOINT: '/api/balance',
     STORAGE_KEY: 'nexaView_wallets',
-    DONATION_WALLET_ADDRESS: 'nexa:nqtsq5g5psnrfuy7mwwe7zelgjdcevme5re2lwuf7gnmy9ts'
+    DONATION_WALLET_ADDRESS: 'nexa:nqtsq5g5vv8nww2wms2wcm2cna2r5lm4cj3avp397dkt4y7f'
 };
 
 export const state = {
